c59bd474325fe19f489a15086de198004b68656e
richardvillaonate <100082193+richardvillaonate@users.noreply.github.com>
2024-12-25 19:43:48 -0500

    Enviar mensajes con y sin imagenes

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,39 @@ import {addKeyword, createBot,createFlow,createProvider, MemoryDB} from '@bot-wh
 import {BaileysProvider, handleCtx} from '@bot-whatsapp/provider-baileys'
 
 const provider = createProvider(BaileysProvider)
-provider.initHttpServer(3002)
-provider.http?.server.post('/message', handleCtx( async (bot,req,res)=>{
-    const body=req.body
-    const phone=body.phone
-    const message=body.message
-    const mediaUrl=body.mediaUrl
-    
+provider.initHttpServer(3003)
+provider.http?.server.post('/message-with-image', handleCtx( async (bot, req, res) => {
+    const body = req.body;
+    const phone = body.phone;
+    const message = body.message;
+    const mediaUrl = body.mediaUrl;
 
-    // console.log(body)
-   
+    console.log(body);
 
-    await bot.sendMessage(phone,message,{
-        media:mediaUrl
-    })
+    // Enviar mensaje con imagen
+    await bot.sendMessage(phone, message, {
+        media: mediaUrl // Aquí es donde se agrega la URL de la imagen
+    });
 
-    res.end()
+    res.end();
+}));
 
-}))
 
-const flowBienvenida = addKeyword('Hola').addAnswer('bienvenid@!');
+provider.http?.server.post('/message-without-image', handleCtx( async (bot, req, res) => {
+    const body = req.body;
+    const phone = body.phone;
+    const message = body.message;
+
+    console.log(body);
+
+    // Enviar solo el mensaje sin imagen
+    await bot.sendMessage(phone, message, {});
+
+    res.end();
+}));
+
+
+const flowBienvenida = addKeyword('TaluuApp').addAnswer('bienvenid@!');
 
 const main = async()=>{
     await createBot({
