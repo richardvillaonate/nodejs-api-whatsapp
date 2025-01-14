f50194826f7707ba562e7678578c6d9c6590588d
richardvillaonate <100082193+richardvillaonate@users.noreply.github.com>
2025-01-14 11:39:54 -0600

    king licores

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,15 @@ import {BaileysProvider, handleCtx} from '@bot-whatsapp/provider-baileys'
 const provider = createProvider(BaileysProvider)
 
 //cambiar puerto para el tenant 
-// casa23restobar puerto 3002 casa
-provider.initHttpServer(3002)
+// casa23restobar puerto 3004 licores kink
+provider.initHttpServer(3004)
 provider.http?.server.post('/message-with-image', handleCtx( async (bot, req, res) => {
     const body = req.body;
     const phone = body.phone;
     const message = body.message;
     const mediaUrl = body.mediaUrl;
 
-    console.log(body);
+    // console.log(body);
 
     // Enviar mensaje con imagen
     await bot.sendMessage(phone, message, {
@@ -37,7 +37,7 @@ provider.http?.server.post('/message-without-image', handleCtx( async (bot, req,
 }));
 
 
-const flowBienvenida = addKeyword('TaluuApp').addAnswer('bienvenid@!');
+const flowBienvenida = addKeyword(['hola', 'Hola', 'buenas', 'Buenas']).addAnswer('Hoia mira nuestros productos https://taluu.app/pedidos/kinglicores ');
 
 const main = async()=>{
     await createBot({
