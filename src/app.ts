bbf4f3f4d36cea801c9d837630fa0f66d0817f56
richardvillaonate <100082193+richardvillaonate@users.noreply.github.com>
2025-02-12 15:51:39 -0500

    tenant-demo

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ import {BaileysProvider, handleCtx} from '@bot-whatsapp/provider-baileys'
 const provider = createProvider(BaileysProvider)
 
 //cambiar puerto para el tenant 
-// casa23restobar puerto 3002 
-provider.initHttpServer(3002)
+// demo puerto 3003
+provider.initHttpServer(3003)
 provider.http?.server.post('/message-with-image', handleCtx( async (bot, req, res) => {
     const body = req.body;
     const phone = body.phone;
@@ -39,31 +39,7 @@ provider.http?.server.post('/message-without-image', handleCtx( async (bot, req,
 
 const flowBienvenida = addKeyword(['hola', 'Hola', 'buenas', 'Buenas'])
     .addAnswer(
-        `¡Hola! 👋  
-
-Link para realizar pedido y ver menú ⬇  
-https://taluu.app/pedidos/casa23  
-
-Para cotizar tu evento ⬇  
-** Nombre:*  
-** Tipo de evento:*  
-** Fecha:*  
-** Hora:*  
-** Número de personas:*  
-** Celular:*  
-** Observaciones:*  
-
-📍 *Horarios*  
-
-*Lunes - Jueves*  
-  🕛 Día: 12:00 pm - 3:00 pm  
-  🌙 Noche: 6:00 pm - 10:00 pm  
-
-*Viernes - Sábado - Domingo*  
-  🕛 Día: 12:00 pm - 4:00 pm  
-  🌙 Noche: 6:00 pm - 11:00 pm  
-
-📍 Calle 7 # 11-99`
+        `¡Hola! 👋 `
     );
 
 
